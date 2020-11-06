fe7c36bcf83b8c56a2b495377b55f17449385b6f
partiadam <hunadam2004@gmail.com>
2020-11-06 07:41:20 +0000

    Initial commit

---
+++ f0003a.py
@@ -0,0 +1,3 @@
+kereszt = input('Mi a keresztneved? ')
+vezeték = input('Mi a vezetékneved? ')
+print('szia ', vezeték , kereszt, ', örülök hogy találkoztunk.', sep='')
---
+++ f0003b.py
@@ -0,0 +1,6 @@
+vezetéknév = input('Mi a keresztneved?')
+keresztnév = input('Mi a vezetékneved?')
+év = input('Melyik évben születtél?')
+csillagjegy = input('Melyik csillagjegyben születtél?')
+print('A te neved ', vezetéknév, ' ', keresztnév, '.', sep='')
+print('Az Úr ' , év, '. évében születtél, a ', csillagjegy, 'jegyében.', sep='')
